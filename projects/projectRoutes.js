97f822b49937fe18a1fc0511d4c652d0dfbb19bf
Jacob Ekegren <JacobEkegren@MacBook-Pro.local>
2020-10-11 16:46:59 -0700

    Added end points for get, put, and post for projects

---
+++ projects/projectRoutes.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../data/helpers/projectModel");
+
+router.get("/", (req, res) => {
+    db.get()
+    .then((projects) => {
+        res.status(200).json(projects);
+    })
+    .catch((err) => {
+        res.status(500).json({
+            message: "An error has occured"
+            });
+    });
+})
+
+router.post("/", (req, res)=>{
+    const {name, description} = req.body;
+    if(!name || !description) 
+    return res.status(400).json({
+        message: "Name and description requried"
+    });
+    db.insert(req.body)
+    .then((newProject) => {
+        res.status(201).json(newProject);
+    })
+    .catch((err) => {
+        res.status(500).json({
+            message: "An error has occured"
+        });
+    })
+});
+
+router.get("/:id", (req, res)=>{
+    db.get(req.params.id)
+    .then((project) => {
+        if(!project) 
+        return res.status(404).json({
+            message: "Project doesn't exist"
+        })
+        res.status(200).json(project);
+    })
+    .catch((err) => {
+        res.status(500).json({
+            message: "An error has occured"
+        });
+    })
+});
+
+router.put("/:id", (req, res)=>{
+    const changes = req.body;
+    if(!changes) 
+    res.status(400).json({
+        messages: "Required info missing"
+    });
+    db.update(req.params.id, changes)
+    .then((updatedProject) => {
+        if(!updatedProject) 
+        return res.status(404).json({
+            message: "Project does not exist"
+        });
+        res.status(201).json(updatedProject);
+    })
+    .catch((err)=>{
+        res.status(500).json({
+            message: "An error has occured"
+        });
+    })
+});
+
+module.exports = router;
